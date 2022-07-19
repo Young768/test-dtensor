f997979dcf68aac57c3c657e80ff98b1523f220f
Donglin Y <513399457@qq.com>
2022-07-19 13:34:51 -0700

    add script.

--- build_imagenet_data.py
+++ build_imagenet_data.py
@@ -79,6 +79,7 @@ import threading
 
 import numpy as np
 import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 tf.app.flags.DEFINE_string('train_directory', '/tmp/',
                            'Training data directory')
