8e22d330ad45a966f7ff04421f9cdd1347b4d7f7
Donglin Y <513399457@qq.com>
2022-10-21 17:50:19 -0700

    add baseline.

--- single-node.py
+++ single-node.py
@@ -19,9 +19,10 @@ gpus = tf.config.list_physical_devices('GPU')
 if gpus:
   for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
-  logical_gpus = tf.config.list_logical_devices('GPU')
-  print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
   tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+  #logical_gpus = tf.config.list_logical_devices('GPU')
+  #print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+
 
 def parse_cmdline(init_vals):
   f = argparse.ArgumentDefaultsHelpFormatter
